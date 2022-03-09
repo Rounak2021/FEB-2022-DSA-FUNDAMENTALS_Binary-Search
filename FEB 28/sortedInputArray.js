de50b1ebf133787eec0704b5122f73fdac0e2f52
Rounak Chakraborty <rounak17chakraborty@gmail.com>
2022-03-09 22:14:26 +0530

    Arrays

---
+++ FEB 28/sortedInputArray.js
@@ -0,0 +1,16 @@
+const twoSum= (numbers, target)=>{
+    let lower = 0;
+    let upper = numbers.length -1;
+    while(upper>lower){
+        let sum = numbers[lower] + numbers[upper];
+        if(sum< target){
+            lower +=1;
+        }else if(sum>target){
+            upper -=1;
+        }else{
+            return [lower + 1, upper + 1];
+        }
+    }
+}
+
+//Leetcode Solution: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/
